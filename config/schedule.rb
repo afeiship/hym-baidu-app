1c44540c280921d302973f00b5f2c8fa3fcb97d6
afeiship <1290657123@qq.com>
2019-03-10 21:58:20 +0800

    add: project path

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,7 +18,8 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+PROJECT_PATH = "./github/hym-baidu-app"
 
-every 1.day, at: ["00:00 am", "9:30 am", "11:00 am", "13:30 pm", "21:56 pm"] do
-  command "ruby ./src/do-sigin.rb"
+every 1.day, at: ["00:00 am", "9:30 am", "11:00 am", "13:30 pm", "21:59 pm"] do
+  command "ruby #{PROJECT_PATH}/src/do-sigin.rb"
 end
